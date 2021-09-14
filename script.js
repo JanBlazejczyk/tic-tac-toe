3a99db183bf24cccad0358dc6fc8252db0a73a06
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-09-14 10:27:37 +0200

    styling start page

---
+++ script.js
@@ -0,0 +1,6 @@
+
+console.log("Script works!")
+const header = document.querySelector(".start-screen");
+const startScreen = document.getElementById("start-screen-template");
+const content = startScreen.content;
+header.appendChild(content);
