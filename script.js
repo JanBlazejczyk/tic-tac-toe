fa1c051c528f9d96189705b5b3e0c04672e4acc7
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-09-15 09:59:06 +0200

    created dummy data for styling the game display screen

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 console.log("Script works!")
-const header = document.querySelector(".start-screen");
+const header = document.querySelector(".display-templates");
 const startScreen = document.getElementById("start-screen-template");
 const content = startScreen.content;
-header.appendChild(content);
+// header.appendChild(content);
+
+const main = document.querySelector(".display-templates");
+const gameHTML = document.getElementById("game-screen-template");
+const gameScreenContent = gameHTML.content;
+main.appendChild(gameScreenContent);
