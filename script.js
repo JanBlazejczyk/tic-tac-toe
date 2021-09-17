41439b67368617a8ea2d0c59cff6067f71899cbf
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-09-17 09:32:17 +0200

    media queries for narrower screens added

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const resultScreenTemplate = document.querySelector("#result-screen-template");
 const resultScreenTemplateContent = resultScreenTemplate.content;
 
 
-// placeholder.appendChild(startScreenTemplateContent);
-placeholder.appendChild(gameScreenTemplateContent);
+placeholder.appendChild(startScreenTemplateContent);
+// placeholder.appendChild(gameScreenTemplateContent);
 // placeholder.appendChild(resultScreenTemplateContent);
 
